f495958b9c5d224d41226d16cb7aa27861bf17df
Keshab Aryal <keshabaryal152@gmail.com>
2025-08-27 17:44:53 +0545

    Add all the templates using formik

---
+++ src/Formik/UseComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UseComponent = () => {
+  return <div>UseComponent</div>;
+};
+
+export default UseComponent;
+
+/* 
+form
+email       required   ----> input:email
+password    required    ----> input:p/w
+fullName    required    ----> input:text
+isMarried               ---> checkbox
+country     required    ----> select
+gender      required    ----> radio
+description             ----> textarea
+*/
